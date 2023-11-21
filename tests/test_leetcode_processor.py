05009c85bfb4fbdceb73526e7cef5549bfa68275
pChitral <chitral.patil@vit.edu.in>
2023-11-21 03:33:14 -0600

    Refactor test_leetcode_processor.py

--- tests/test_leetcode_processor.py
+++ tests/test_leetcode_processor.py
@@ -1,21 +1,58 @@
 import unittest
-from unittest.mock import Mock
-from src.leetcode_processor import LeetCodeProcessor
-from src.openai_client import OpenAIClient
+from unittest.mock import MagicMock
+from openai_client import OpenAIClient
+from leetcode_processor import LeetCodeProcessor
 
 
 class TestLeetCodeProcessor(unittest.TestCase):
     def setUp(self):
-        # Mock the OpenAIClient for testing
-        self.mock_client = Mock(spec=OpenAIClient)
+        self.mock_client = MagicMock(spec=OpenAIClient)
         self.processor = LeetCodeProcessor(self.mock_client)
 
-    def test_process_problem(self):
-        # Test the process_problem method
-        # Again, this is a placeholder test and will require mocking
-        self.assertIsNotNone(self.processor.process_problem(1))
+    def test_init(self):
+        self.assertIsInstance(self.processor.client, OpenAIClient)
 
-    # Add more tests for other methods of LeetCodeProcessor
+    def test_pretty_print_with_valid_messages(self):
+        messages = [
+            {"role": "assistant", "content": [{"text": {"value": "test message"}}]}
+        ]
+        result = self.processor.pretty_print(messages)
+        self.assertEqual(result, "test message")
+
+    def test_pretty_print_with_empty_messages(self):
+        result = self.processor.pretty_print([])
+        self.assertIsNone(result)
+
+    def test_pretty_print_with_no_assistant_role(self):
+        messages = [{"role": "user", "content": [{"text": {"value": "test message"}}]}]
+        result = self.processor.pretty_print(messages)
+        self.assertIsNone(result)
+
+    def test_pretty_print_with_invalid_messages(self):
+        messages = [{"role": "assistant"}]  # No 'content' field
+        with self.assertRaises(Exception):
+            self.processor.pretty_print(messages)
+
+    def test_process_problem_with_valid_problem_number(self):
+        self.mock_client.create_thread_and_run.return_value = ("thread_id", "run_id")
+        self.mock_client.wait_on_run.return_value = "run_id"
+        self.mock_client.get_response.return_value = [
+            {"role": "assistant", "content": [{"text": {"value": "Problem solved"}}]}
+        ]
+
+        problem_number, output = self.processor.process_problem(1)
+        self.assertEqual(problem_number, 1)
+        self.assertEqual(output, "Problem solved")
+
+    def test_process_problem_with_invalid_problem_number(self):
+        problem_number, output = self.processor.process_problem(-1)
+        self.assertIsNone(output)
+
+    def test_process_problem_with_exception(self):
+        self.mock_client.create_thread_and_run.side_effect = Exception("Error")
+
+        problem_number, output = self.processor.process_problem(1)
+        self.assertIsNone(output)
 
 
 if __name__ == "__main__":
