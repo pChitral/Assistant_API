b202df13dd11241edf3534af0e3e99bec9a8232e
pChitral <chitral.patil@vit.edu.in>
2023-11-21 13:12:44 -0600

    need to work on tests

--- tests/test_leetcode_processor.py
+++ tests/test_leetcode_processor.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
 import unittest
 from unittest.mock import MagicMock
 from openai_client import OpenAIClient
